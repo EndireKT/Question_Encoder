196433761be67e91414faf88f00264d6a99485a6
EndireKT <72508923+EndireKT@users.noreply.github.com>
2022-05-26 13:12:49 +0800

    Set up

---
+++ src/main/java/projectname/Main.java
@@ -0,0 +1,16 @@
+package projectname;
+
+public class Main {
+
+    public String encode(String plainText) {
+        return null;
+    }
+
+    public String decode(String encodedText) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
